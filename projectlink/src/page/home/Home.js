d3c1a6da7b4936c3836b4cb323960fd24ab45ca6
Kwak <mjkwahk@gmail.com>
2024-07-31 10:50:21 +0900

    로그인 및 회원가입 페이지 css

--- projectlink/src/page/home/Home.js
+++ projectlink/src/page/home/Home.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
 import logo from "../../assets/logo.png";
-import "../styles.css";
 import "../../components/Button.css";
 import Button from '../../components/Button';
 import { ProfileCardList } from "../../components/profilecard/ProfileCard";
--- projectlink/src/page/login/Login.js
+++ projectlink/src/page/login/Login.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Card from "../../components/Card";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import '../login/Login.css';
 
 export default function Login() {
     const [formState, setFormState] = useState({
@@ -44,17 +45,22 @@ export default function Login() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <Card
-                placeholders={[
-                    { type: "text", text: "아이디", name: "username", onChange: handleInputChange },
-                    { type: "password", text: "비밀번호", name: "password", onChange: handleInputChange }
-                ]}
-                buttonText="로그인"
-                linkText="회원이 아니신가요?"
-                linkUrl="/signup"
-                onChange={handleInputChange}
-            />
-        </form>
+        <div className="login-container">
+            <div className="signup-link">
+                <Link to="/signup">No Account yet?</Link>
+                <li className="nav-item">
+                    <Link to="/signup" className="signup-btn">SIGN UP</Link>
+                </li>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <Card
+                    placeholders={[
+                        { type: "text", text: "ID", name: "username", onChange: handleInputChange },
+                        { type: "password", text: "PASSWORD", name: "password", onChange: handleInputChange }
+                    ]}
+                    buttonText="LOGIN"
+                />
+            </form>
+        </div>
     );
 }
